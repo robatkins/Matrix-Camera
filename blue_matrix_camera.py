d051c2560c1dd92ceae73cff283f7c566dec2290
Rob Atkins <rbatkins101@gmail.com>
2023-12-12 18:20:49 -0500

    Added blue and red variants

---
+++ blue_matrix_camera.py
@@ -0,0 +1,97 @@
+import cv2
+import numpy as np
+import pygame as pg
+from pygame.locals import QUIT
+
+class Matrix:
+    def __init__(self, app, font_size=8):
+        self.app = app
+        self.FONT_SIZE = font_size
+        self.SIZE = self.ROWS, self.COLS = app.HEIGHT // font_size, app.WIDTH // font_size
+        self.katakana = np.array([chr(int('0x30a0', 16) + i) for i in range(96)] + ['' for i in range(10)])
+        self.font = pg.font.Font('font/ms mincho.ttf', font_size)
+
+        self.matrix = np.random.choice(self.katakana, self.SIZE)
+        self.char_intervals = np.random.randint(25, 50, size=self.SIZE)
+        self.cols_speed = np.random.randint(1, 500, size=self.SIZE)
+        self.prerendered_chars = self.get_prerendered_chars()
+
+    def get_frame(self):
+        _, frame = self.app.cam.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.flip(frame, 1)  # Flip vertically
+        frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)  # Rotate 90 degrees clockwise
+        frame = pg.surfarray.make_surface(frame)
+        frame = pg.transform.scale(frame, self.app.RES)
+        pixel_array = pg.pixelarray.PixelArray(frame)
+        return pixel_array
+
+    def get_prerendered_chars(self):
+        char_colors = [(0, 0, blue) for blue in range(256)]
+        prerendered_chars = {}
+        for char in self.katakana:
+            prerendered_char = {(char, color): self.font.render(char, True, color) for color in char_colors}
+            prerendered_chars.update(prerendered_char)
+        return prerendered_chars
+
+    def run(self):
+        frames = pg.time.get_ticks()
+        self.change_chars(frames)
+        self.shift_column(frames)
+        self.draw()
+
+    def shift_column(self, frames):
+        num_cols = np.argwhere(frames % self.cols_speed == 0)
+        num_cols = num_cols[:, 1]
+        num_cols = np.unique(num_cols)
+        self.matrix[:, num_cols] = np.roll(self.matrix[:, num_cols], shift=1, axis=0)
+
+    def change_chars(self, frames):
+        mask = np.argwhere(frames % self.char_intervals == 0)
+        new_chars = np.random.choice(self.katakana, mask.shape[0])
+        self.matrix[mask[:, 0], mask[:, 1]] = new_chars
+
+    def draw(self):
+        self.image = self.get_frame()
+        for y, row in enumerate(self.matrix):
+            for x, char in enumerate(row):
+                if char:
+                    pos = x * self.FONT_SIZE, y * self.FONT_SIZE
+                    _, red, green, blue = pg.Color(self.image[pos])
+                    if red and green and blue:
+                        color = (red + green + blue) // 3
+                        color = 220 if 160 < color < 220 else color
+                        char = self.prerendered_chars[(char, (0, 0, color))]
+                        char.set_alpha(color + 60)
+                        self.app.surface.blit(char, pos)
+
+class MatrixVision:
+    def __init__(self):
+        self.RES = self.WIDTH, self.HEIGHT = 960, 720
+        pg.init()
+        pg.display.set_caption("The Matrix")
+        self.screen = pg.display.set_mode(self.RES)
+        self.surface = pg.Surface(self.RES)
+        self.clock = pg.time.Clock()
+        self.cam = cv2.VideoCapture(0)
+        self.matrix = Matrix(self)
+
+    def draw(self):
+        self.surface.fill(pg.Color('black'))
+        self.matrix.run()
+        self.screen.blit(self.surface, (0, 0))
+
+    def run(self):
+        while True:
+            self.draw()
+            for event in pg.event.get():
+                if event.type == QUIT:
+                    self.cam.release()
+                    pg.quit()
+                    return
+            pg.display.flip()
+            self.clock.tick(30)
+
+if __name__ == '__main__':
+    app = MatrixVision()
+    app.run()
